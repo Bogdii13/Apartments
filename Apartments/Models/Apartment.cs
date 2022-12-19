260e3c107b30ecf0fffdda9de885c60c8112be56
Bogdii13 <bogdanionutsas13@gmail.com>
2022-12-19 16:04:03 +0200

    Add project files.

---
+++ Apartments/Models/Apartment.cs
@@ -0,0 +1,11 @@
+﻿namespace Apartments.Models
+{
+    public class Apartment
+    {
+
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public decimal Price { get; set; }
+    }
+}
