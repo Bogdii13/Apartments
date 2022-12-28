a3ebea0418311f5b549f7351ac6dac0d26debd66
Bogdii13 <bogdanionutsas13@gmail.com>
2022-12-28 14:24:38 +0200

    part2

--- Apartments/Models/ApartmentData.cs
+++ Apartments/Models/ApartmentData.cs
@@ -2,5 +2,9 @@
 {
     public class ApartmentData
     {
+
+        public IEnumerable<Apartment> Apartments { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+        public IEnumerable<Apartamentcategory> Apartamentcategories { get; set; }
     }
 }
